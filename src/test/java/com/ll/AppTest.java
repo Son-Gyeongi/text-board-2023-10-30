2fc4c0669a740e305a4a68b80bbfe91b29f0ff93
Son-Gyeongi <thsruddl77@gmail.com>
2023-10-31 03:04:11 +0900

    quotation 12step / ãregist

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -8,10 +8,29 @@ import java.util.Scanner;
 public class AppTest {
 
     @Test
-    @DisplayName("종료를 누르면 꺼진다.")
+    @DisplayName("종료를 입력하면 꺼진다.")
     void t1() {
         // Scanner를 직접 만들면(키보드로 입력받는) 안되고 우리가 만들어야 한다.
         Scanner scanner = TestUtil.genScanner("""
+                종료
+                """.stripIndent());
+
+        // Scanner를 만들어서 집어넣는다.
+        new App(scanner).run();
+
+        scanner.close();
+
+        System.out.println("프로그램이 종료되었습니다.");
+    }
+
+    @Test
+    @DisplayName("등록을 입력하면 명언과 작가를 입력받는다.")
+    void t2() {
+        // Scanner를 직접 만들면(키보드로 입력받는) 안되고 우리가 만들어야 한다.
+        Scanner scanner = TestUtil.genScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
                 종료
                 """.stripIndent());
 
